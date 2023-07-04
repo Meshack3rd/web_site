614c102523417cfb0af03682c693c9cb3d623124
Meshack3rd <135335228+Meshack3rd@users.noreply.github.com>
2023-07-04 12:33:41 +0300

    Initial commit

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
 
+menu.addEventListener('click',function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+    
+}); 
+
+const logregBox = document.querySelector('.logreg-box');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click',() => {logregBox.classList.add('active');
+});
+
+loginLink.addEventListener('click',() => {logregBox.classList.remove('active');
+});
